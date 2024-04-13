fe51ef82c7f57389088a74cd476b0e749dccae03
Jordan <jordan.l.partridge@gmail.com>
2024-04-12 22:19:09 -0700

    workflows and dusk

---
+++ tests/Browser/NavigationTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use Laravel\Dusk\Browser;
+
+it('shows the right name', function () {
+    $this->browse(function (Browser $browser) {
+        $browser->visit('/')
+                ->assertSee('Jordan Partridge');
+    });
+});
+
+it('can follow the engineering link', function () {
+    $this->browse(function (Browser $browser) {
+        $browser->visit('/')
+                ->click('@nav-engineering')
+                ->assertPathIs('/engineering');
+    });
+});
+
+it('can follow the home link', function () {
+    $this->browse(function (Browser $browser) {
+        $browser->visit('/engineering')
+                ->click('@nav-home')
+                ->assertPathIs('/');
+    });
+});
+
+it('can follow the admin link', function () {
+    $this->browse(function (Browser $browser) {
+        $browser->visit('/')
+                ->click('@nav-admin')
+                ->assertPathIs('/admin/login');
+    });
+});
