8d68367790fb76e5e05cd98807977dfcf9acde5c
Jordan <jordan.l.partridge@gmail.com>
2024-04-13 08:13:57 -0700

    more organized home page

---
+++ resources/views/Components/hero.blade.php
@@ -0,0 +1,49 @@
+<section class="hero is-palette-dark is-medium">
+    <!-- Hero head: will stick at the top -->
+    <div class="hero-head">
+        <nav class="navbar">
+            <div class="container">
+                <div class="navbar-brand">
+                    <figure class="image is-32x32 m-1 container">
+                        <img class="is-rounded" src="/img/hero.gif" />
+                    </figure>
+                    <span class="navbar-burger" data-target="navbarMenuHeroA">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+                </div>
+                <div id="navbarMenuHeroA" class="navbar-menu">
+                    <div class="navbar-end">
+                       <x-nav/>
+
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <!-- Hero content: will be in the middle -->
+    <div class="hero-body">
+        <div class="container has-text-centered">
+            {{ $slot}}
+        </div>
+    </div>
+
+    <!-- Hero footer: will stick at the bottom -->
+    <div class="hero-foot">
+        <nav class="tabs">
+            <div class="container">
+                <ul>
+                    <li class="is-active"><a>Software Engineering</a></li>
+                    <li><x-link href="/">Home</x-link></li>
+                    <li><a>Grid</a></li>
+                    <li><a>Elements</a></li>
+                    <li><a>Components</a></li>
+                    <li><a>Layout</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</section>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,20 +1,14 @@
 <x-layout>
-    <section class="hero">
-        <div class="hero-body container">
-            <video width="300" src="/img/hero.mp4" autoplay loop muted></video>
+    <x-hero>
+        <div class="container">
+            <div class="container is-rounded">
+           <video width="300" src="/img/hero.mp4" autoplay loop muted style="
+                  object-fit: cover;
+                  border-radius: 100px"
+                  ></video>
+            </div>
             <p class="title">Jordan Partridge</p>
-            <p class="subtitle">Engineer | Fat Bike Enthusiast | Foodie</p>
+            <p class="subtitle">Software Engineer | Fat Bike Fattie | Foodie</p>
         </div>
-    </section>
-    <h1>Home Page</h1>
-    <div class="block">
-        This text is within a <strong>block</strong>.
-    </div>
-    <div class="block">
-        This text is within a <strong>second block</strong>. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-    </div>
-    <div class="block">
-        This text is within a <strong>third block</strong>. This block has no margin at the bottom.
-    </div>
+    </x-hero>
 </x-layout>
