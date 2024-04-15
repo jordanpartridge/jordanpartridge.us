d26e95d19669d4d5ae583ad9f6de9c2ef328f94a
Jordan <jordan.l.partridge@gmail.com>
2024-04-14 18:31:15 -0700

    remove app.css from layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{config('app.name')}}</title>
-    @vite(['resource/assets/css/app.css', 'resource/assets/js/app.js', 'resource/assets/css/app.scss'])
+    @vite(['resource/assets/js/app.js', 'resource/assets/css/app.scss'])
     <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css"
