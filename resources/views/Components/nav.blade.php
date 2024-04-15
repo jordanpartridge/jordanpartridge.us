48b896ab8f3260dff2d21e7f4ea9f2cf5be4f2e8
Jordan <jordan.l.partridge@gmail.com>
2024-04-14 18:07:13 -0700

    add links to hero

--- resources/views/Components/nav.blade.php
+++ resources/views/Components/nav.blade.php
@@ -1,8 +1,3 @@
 <x-link href="/">Home</x-link>
 <x-link href="/engineering">Engineering</x-link>
-<a class="button" href="https://github.com/jordanpartridge">
-    <span class="icon">
-      <i class="fab fa-github"></i>
-    </span>
-    <span>GitHub</span>
-</a>
+
--- resources/views/pages/engineering/index.blade.php
+++ resources/views/pages/engineering/index.blade.php
@@ -1,9 +1,15 @@
 <x-hero>
     <div class="container">
-        <video  class="rounded" width="300" src="/img/hero.mp4" autoplay loop muted></video>
-        <p class="title">Engineering</p>
-        <p class="subtitle">Engineer | Fat Bike Fattie | Foodie</p>
+        <div class="container is-rounded">
+            <video width="300" src="/img/hero.mp4" autoplay loop muted style="
+                  object-fit: cover;
+                  border-radius: 100px"
+            ></video>
+        </div>
+        <p class="title">Jordan Partridge</p>
+        <p class="subtitle">Software Engineer | Fat Bike Fattie | Foodie</p>
     </div>
-</x-hero><x-layout>
+</x-hero>
+<x-layout>
 
 </x-layout>
