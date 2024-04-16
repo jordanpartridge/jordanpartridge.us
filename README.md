50eb145e95783d15281172b5844c4d348066a496
Jordan <jordan.l.partridge@gmail.com>
2024-04-15 20:40:14 -0700

    Update site to use genesis starter kit
    base site structure
    
    linked in link added
    
    add forge badge to readme
    
    update tests workflow
    
    fix pint
    
    update test workflow (no nova)
    
    update badges
    
    more on readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jordanpartridge.us
-<h1>This is my starting point for my website</h1>
+# My Personal Website, Portfolio, and Blog and other adventures.
+[![Laravel Forge Site Deployment Status](https://img.shields.io/endpoint?url=https%3A%2F%2Fforge.laravel.com%2Fsite-badges%2Ff5322d8d-6b11-49f0-9272-e0302c67842f%3Fdate%3D1%26commit%3D1&style=plastic)](https://forge.laravel.com/servers/714274/sites/2318112)
+[![Pint](https://github.com/jordanpartridge/jordanpartridge.us/actions/workflows/Pint.yml/badge.svg?branch=master)](https://github.com/jordanpartridge/jordanpartridge.us/actions/workflows/Pint.yml)
+[![Tests](https://github.com/jordanpartridge/jordanpartridge.us/actions/workflows/Tests.yml/badge.svg)](https://github.com/jordanpartridge/jordanpartridge.us/actions/workflows/Tests.yml)
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,12 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Factories;
 
 use App\Models\Post;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class PostFactory extends Factory
+/**
+ * @extends Factory<\App\Models\Post>
+ */
+final class PostFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
@@ -21,12 +25,20 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => $this->faker->sentence(4),
-            'slug' => $this->faker->slug(),
-            'content' => $this->faker->paragraphs(3, true),
-            'published' => $this->faker->boolean(),
-            'published_at' => $this->faker->dateTime(),
-            'user_id' => User::factory(),
+            'user_id' => \App\Models\User::factory(),
+            'title' => fake()->sentence(),
+            'body' => fake()->paragraphs(10, true),
+            'image' => 'https://picsum.photos/1280/720?random='.fake()->unique()->slug(),
+            'slug' => fake()->unique()->slug(),
+            'excerpt' => fake()->optional()->word,
+            'type' => fake()->word,
+            'status' => 'published',
+            'active' => 1,
+            'featured' => fake()->boolean,
+            'meta_title' => fake()->optional()->word,
+            'meta_description' => fake()->optional()->word,
+            'meta_schema' => fake()->optional()->text,
+            'meta_data' => fake()->optional()->text,
         ];
     }
 }
