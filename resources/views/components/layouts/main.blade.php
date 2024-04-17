336e99e5d341eeeac0f8c7db64bee40c5564e57f
Jordan <jordan.l.partridge@gmail.com>
2024-04-16 20:08:40 -0700

    Initial Strava Integration setup.

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -16,7 +16,7 @@
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <title>{{ $title ?? 'Genesis' }}</title>
+        <title>{{ $title ?? 'Jordan Partridge' }}</title>
     </head>
     <body class="min-h-screen antialiased bg-white dark:bg-gradient-to-b dark:from-gray-950 dark:to-gray-900">
         {{ $slot }}
