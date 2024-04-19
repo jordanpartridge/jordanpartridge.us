d0d8e1681d7ab6d7ccce61353c64db41412cf2c4
Jordan <jordan.l.partridge@gmail.com>
2024-04-18 20:24:25 -0700

    login link and fix underdog

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate([
+            'email' => 'test@jordanpartridge.us',
+        ], [
+            'name' => 'Jordan Partridge',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -10,7 +10,7 @@ state(['email' => '', 'password' => '', 'remember' => false]);
 rules(['email' => 'required|email', 'password' => 'required']);
 name('login');
 
-$authenticate = function(){
+$authenticate = function () {
     $this->validate();
 
     if (!Auth::attempt(['email' => $this->email, 'password' => $this->password], $this->remember)) {
@@ -18,7 +18,7 @@ $authenticate = function(){
 
         return;
     }
-    
+
     event(new Login(auth()->guard('web'), User::where('email', $this->email)->first(), $this->remember));
 
     return redirect()->intended('/');
@@ -32,10 +32,11 @@ $authenticate = function(){
 
         <div class="sm:mx-auto sm:w-full sm:max-w-md">
             <x-ui.link href="{{ route('home') }}">
-                <x-ui.logo class="w-auto h-10 mx-auto text-gray-700 fill-current dark:text-gray-100" />
+                <x-ui.logo class="w-auto h-10 mx-auto text-gray-700 fill-current dark:text-gray-100"/>
             </x-ui.link>
 
-            <h2 class="mt-5 text-2xl font-extrabold leading-9 text-center text-gray-800 dark:text-gray-200">Sign in to your account</h2>
+            <h2 class="mt-5 text-2xl font-extrabold leading-9 text-center text-gray-800 dark:text-gray-200">Sign in to
+                your account</h2>
             <div class="text-sm leading-5 text-center text-gray-600 dark:text-gray-400 space-x-0.5">
                 <span>Or</span>
                 <x-ui.text-link href="{{ route('register') }}">create a new account</x-ui.text-link>
@@ -43,24 +44,29 @@ $authenticate = function(){
         </div>
 
         <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-            <div class="px-10 py-0 sm:py-8 sm:shadow-sm sm:bg-white dark:sm:bg-gray-950/50 dark:border-gray-200/10 sm:border sm:rounded-lg border-gray-200/60">
-                @volt('auth.login')
-                    <form wire:submit="authenticate" class="space-y-6">
-                        
-                        <x-ui.input label="Email address" type="email" id="email" name="email" wire:model="email" />
-                        <x-ui.input label="Password" type="password" id="password" name="password" wire:model="password" />
+                    <div
+                        class="px-10 py-0 sm:py-8 sm:shadow-sm sm:bg-white dark:sm:bg-gray-950/50 dark:border-gray-200/10 sm:border sm:rounded-lg border-gray-200/60">
+                        @volt('auth.login')
 
-                        <div class="flex items-center justify-between mt-6 text-sm leading-5">
-                            <x-ui.checkbox label="Remember me" id="remember" name="remember" wire:model="remember" />
-                            <x-ui.text-link href="{{ route('password.request') }}">Forgot your password?</x-ui.text-link>
-                        </div>
+                        <form wire:submit="authenticate" class="space-y-6">
 
-                        <x-ui.button type="primary" rounded="md" submit="true">Sign in</x-ui.button>
-                    </form>
-                @endvolt
-            </div>
+                            <x-ui.input label="Email address" type="email" id="email" name="email" wire:model="email"/>
+                            <x-ui.input label="Password" type="password" id="password" name="password"
+                                        wire:model="password"/>
+
+
+                            <div class="flex items-center justify-between mt-6 text-sm leading-5">
+                                <x-ui.checkbox label="Remember me" id="remember" name="remember" wire:model="remember"/>
+                                <x-ui.text-link href="{{ route('password.request') }}">Forgot your password?
+                                </x-ui.text-link>
+                            </div>
+                            <x-ui.button type="primary" rounded="md" submit="true">Sign in</x-ui.button>
+                        </form>
+
+                        @endvolt
+                    </div>
+                </div>
+
         </div>
-        
-    </div>
 
 </x-layouts.main>
