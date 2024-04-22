bcc3712be8af653731c9d6882f945b4e9f755430
Jordan <jordan.l.partridge@gmail.com>
2024-04-21 21:04:59 -0700

    Style adjustments
    weekly stats added
    
    pint

--- resources/views/pages/bike/index.blade.php
+++ resources/views/pages/bike/index.blade.php
@@ -6,6 +6,14 @@ use function Livewire\Volt\{state, rules};
 name('bike');
 middleware(['redirect-to-dashboard']);
 
+\Livewire\Volt\mount(function () {
+    $this->rides              = \App\Models\Ride::whereDate('date', '>', now()->subWeek());
+    $this->weeklyMileage      = number_format($this->rides->sum('distance') * 0.000621371, 1);
+    $this->weeklyAverageSpeed = $this->rides->avg('average_speed') * 2.23694;
+    $this->weeklyMaxSpeed     = $this->rides->max('max_speed') * 2.23694;
+}
+);
+
 ?>
 
 <x-layouts.marketing>
@@ -13,32 +21,32 @@ middleware(['redirect-to-dashboard']);
     <div class="relative flex flex-col items-center justify-center w-full h-auto overflow-hidden" x-cloak>
 
         <svg
-            class="absolute top-0 left-0 w-7/12 -ml-40 -translate-x-1/2 fill-current opacity-10 dark:opacity-5 text-slate-400"
-            viewBox="0 0 978 615" fill="none" xmlns="http://www.w3.org/2000/svg">
+                class="absolute top-0 left-0 w-7/12 -ml-40 -translate-x-1/2 fill-current opacity-10 dark:opacity-5 text-slate-400"
+                viewBox="0 0 978 615" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
-                d="M978 216.141C656.885 277.452 321.116 341.682 0 402.993c39.425-4.071 128.449-11.563 167.843-15.912l6.661 22.46c59.138 174.752 275.144 254.906 438.792 172.235 48.902-72.088 119.911-180.018 171.073-255.946L978 216.141ZM611.485 405.155c-19.059 27.934-46.278 66.955-65.782 94.576-98.453 40.793-230.472-11.793-268.175-111.202-1.096-2.89-1.702-5.965-3.379-11.972l382.99-38.6c-16.875 24.845-31.224 46.049-45.654 67.198Z"/>
+                    d="M978 216.141C656.885 277.452 321.116 341.682 0 402.993c39.425-4.071 128.449-11.563 167.843-15.912l6.661 22.46c59.138 174.752 275.144 254.906 438.792 172.235 48.902-72.088 119.911-180.018 171.073-255.946L978 216.141ZM611.485 405.155c-19.059 27.934-46.278 66.955-65.782 94.576-98.453 40.793-230.472-11.793-268.175-111.202-1.096-2.89-1.702-5.965-3.379-11.972l382.99-38.6c-16.875 24.845-31.224 46.049-45.654 67.198Z"/>
             <path
-                d="m262.704 306.481 1.336-28.817c.25-1.784.572-3.562.951-5.323 17.455-81.121 65.161-136.563 144.708-159.63 81.813-23.725 157.283-5.079 211.302 61.02 6.466 7.912 23.695 33.305 23.695 33.305s107.788-20.295 102.487-22.242C710.939 81.362 569.507-31.34 398.149 8.04 221.871 48.55 144.282 217.1 160.797 331.317c23.221-5.568 78.863-19.192 101.907-24.836Z"/>
+                    d="m262.704 306.481 1.336-28.817c.25-1.784.572-3.562.951-5.323 17.455-81.121 65.161-136.563 144.708-159.63 81.813-23.725 157.283-5.079 211.302 61.02 6.466 7.912 23.695 33.305 23.695 33.305s107.788-20.295 102.487-22.242C710.939 81.362 569.507-31.34 398.149 8.04 221.871 48.55 144.282 217.1 160.797 331.317c23.221-5.568 78.863-19.192 101.907-24.836Z"/>
             <path
-                d="M890.991 458.296c-57.168 2.205-69.605 14.641-71.809 71.809-2.205-57.168-14.641-69.604-71.809-71.809 57.168-2.204 69.604-14.641 71.809-71.809 2.204 57.169 14.641 69.605 71.809 71.809Z"/>
+                    d="M890.991 458.296c-57.168 2.205-69.605 14.641-71.809 71.809-2.205-57.168-14.641-69.604-71.809-71.809 57.168-2.204 69.604-14.641 71.809-71.809 2.204 57.169 14.641 69.605 71.809 71.809Z"/>
             <path
-                d="M890.991 458.296c-57.168 2.205-69.605 14.641-71.809 71.809-2.205-57.168-14.641-69.604-71.809-71.809 57.168-2.204 69.604-14.641 71.809-71.809 2.204 57.169 14.641 69.605 71.809 71.809Z"/>
+                    d="M890.991 458.296c-57.168 2.205-69.605 14.641-71.809 71.809-2.205-57.168-14.641-69.604-71.809-71.809 57.168-2.204 69.604-14.641 71.809-71.809 2.204 57.169 14.641 69.605 71.809 71.809Z"/>
             <path
-                d="M952.832 409.766c-21.048.812-25.626 5.39-26.438 26.438-.811-21.048-5.39-25.626-26.437-26.438 21.047-.811 25.626-5.39 26.437-26.437.812 21.047 5.39 25.626 26.438 26.437Z"/>
+                    d="M952.832 409.766c-21.048.812-25.626 5.39-26.438 26.438-.811-21.048-5.39-25.626-26.437-26.438 21.047-.811 25.626-5.39 26.437-26.437.812 21.047 5.39 25.626 26.438 26.437Z"/>
         </svg>
         <svg
-            class="absolute top-0 right-0 w-7/12 -mr-40 translate-x-1/2 fill-current opacity-10 dark:opacity-5 text-slate-400"
-            viewBox="0 0 978 615" fill="none" xmlns="http://www.w3.org/2000/svg">
+                class="absolute top-0 right-0 w-7/12 -mr-40 translate-x-1/2 fill-current opacity-10 dark:opacity-5 text-slate-400"
+                viewBox="0 0 978 615" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
-                d="M978 216.141C656.885 277.452 321.116 341.682 0 402.993c39.425-4.071 128.449-11.563 167.843-15.912l6.661 22.46c59.138 174.752 275.144 254.906 438.792 172.235 48.902-72.088 119.911-180.018 171.073-255.946L978 216.141ZM611.485 405.155c-19.059 27.934-46.278 66.955-65.782 94.576-98.453 40.793-230.472-11.793-268.175-111.202-1.096-2.89-1.702-5.965-3.379-11.972l382.99-38.6c-16.875 24.845-31.224 46.049-45.654 67.198Z"/>
+                    d="M978 216.141C656.885 277.452 321.116 341.682 0 402.993c39.425-4.071 128.449-11.563 167.843-15.912l6.661 22.46c59.138 174.752 275.144 254.906 438.792 172.235 48.902-72.088 119.911-180.018 171.073-255.946L978 216.141ZM611.485 405.155c-19.059 27.934-46.278 66.955-65.782 94.576-98.453 40.793-230.472-11.793-268.175-111.202-1.096-2.89-1.702-5.965-3.379-11.972l382.99-38.6c-16.875 24.845-31.224 46.049-45.654 67.198Z"/>
             <path
-                d="m262.704 306.481 1.336-28.817c.25-1.784.572-3.562.951-5.323 17.455-81.121 65.161-136.563 144.708-159.63 81.813-23.725 157.283-5.079 211.302 61.02 6.466 7.912 23.695 33.305 23.695 33.305s107.788-20.295 102.487-22.242C710.939 81.362 569.507-31.34 398.149 8.04 221.871 48.55 144.282 217.1 160.797 331.317c23.221-5.568 78.863-19.192 101.907-24.836Z"/>
+                    d="m262.704 306.481 1.336-28.817c.25-1.784.572-3.562.951-5.323 17.455-81.121 65.161-136.563 144.708-159.63 81.813-23.725 157.283-5.079 211.302 61.02 6.466 7.912 23.695 33.305 23.695 33.305s107.788-20.295 102.487-22.242C710.939 81.362 569.507-31.34 398.149 8.04 221.871 48.55 144.282 217.1 160.797 331.317c23.221-5.568 78.863-19.192 101.907-24.836Z"/>
             <path
-                d="M890.991 458.296c-57.168 2.205-69.605 14.641-71.809 71.809-2.205-57.168-14.641-69.604-71.809-71.809 57.168-2.204 69.604-14.641 71.809-71.809 2.204 57.169 14.641 69.605 71.809 71.809Z"/>
+                    d="M890.991 458.296c-57.168 2.205-69.605 14.641-71.809 71.809-2.205-57.168-14.641-69.604-71.809-71.809 57.168-2.204 69.604-14.641 71.809-71.809 2.204 57.169 14.641 69.605 71.809 71.809Z"/>
             <path
-                d="M890.991 458.296c-57.168 2.205-69.605 14.641-71.809 71.809-2.205-57.168-14.641-69.604-71.809-71.809 57.168-2.204 69.604-14.641 71.809-71.809 2.204 57.169 14.641 69.605 71.809 71.809Z"/>
+                    d="M890.991 458.296c-57.168 2.205-69.605 14.641-71.809 71.809-2.205-57.168-14.641-69.604-71.809-71.809 57.168-2.204 69.604-14.641 71.809-71.809 2.204 57.169 14.641 69.605 71.809 71.809Z"/>
             <path
-                d="M952.832 409.766c-21.048.812-25.626 5.39-26.438 26.438-.811-21.048-5.39-25.626-26.437-26.438 21.047-.811 25.626-5.39 26.437-26.437.812 21.047 5.39 25.626 26.438 26.437Z"/>
+                    d="M952.832 409.766c-21.048.812-25.626 5.39-26.438 26.438-.811-21.048-5.39-25.626-26.437-26.438 21.047-.811 25.626-5.39 26.437-26.437.812 21.047 5.39 25.626 26.438 26.437Z"/>
         </svg>
 
         <div class="flex items-center w-full max-w-6xl px-8 pt-12 pb-20 mx-auto">
@@ -49,15 +57,39 @@ middleware(['redirect-to-dashboard']);
                 </div>
                 <div class="m-2 p-2">
                     <div
-                        style="background-image:linear-gradient(160deg,rgba(20,208,136,0.43),#3566e3 50%,#73f4f8, #110e0f)"
-                        class="inline-block w-auto p-0.5 shadow rounded-full animate-gradient">
+                            style="background-image:linear-gradient(160deg,rgba(20,208,136,0.43),#3566e3 50%,#73f4f8, #110e0f)"
+                            class="inline-block w-auto p-0.5 shadow rounded-full animate-gradient">
                         <p class="w-auto h-full px-3 bg-slate-50 dark:bg-neutral-900 dark:text-white py-1.5 font-medium text-sm tracking-widest uppercase  rounded-full text-slate-800/90 group-hover:text-white/100">
                             Bike Joy</p>
                     </div>
                 </div>
+
                 <h1 class="text-3xl font-normal leading-normal text-center text-slate-800 dark:text-white sm:text-4xl lg:text-5xl shadow-sm">
-                    Everyone is entitled to bike joy, some don't want it though.
+                    Everyone is entitled to bike joy.
                 </h1>
+                <div id="dashboard" class="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6 mt-8">
+                    <h2 class="text-xl font-semibold text-gray-800 dark:text-white mb-4 text-center">Weekly
+                        Statistics</h2>
+                    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                        <div class="item bg-white dark:bg-gray-700 rounded-lg p-6 shadow">
+                            <h2 class="text-xl font-semibold text-gray-800 dark:text-white mb-4"> Mileage</h2>
+                            <p class="text-lg text-gray-600 dark:text-gray-300">{{ number_format($this->weeklyMileage, 1) }}
+                                miles</p>
+                        </div>
+                        <div class="item bg-white dark:bg-gray-700 rounded-lg p-6 shadow">
+                            <h2 class="text-xl font-semibold text-gray-800 dark:text-white mb-4">Average Speed Per Ride
+                                Speed</h2>
+                            <p class="text-lg text-gray-600 dark:text-gray-300">{{ number_format($this->weeklyAverageSpeed, 1) }}
+                                mph</p>
+                        </div>
+                        <div class="item bg-white dark:bg-gray-700 rounded-lg p-6 shadow">
+                            <h2 class="text-xl font-semibold text-gray-800 dark:text-white mb-4">Average Max Speed Per Ride</h2>
+                            <p class="text-lg text-gray-600 dark:text-gray-300">{{ number_format($this->weeklyMaxSpeed, 1) }}
+                                mph</p>
+                        </div>
+                        <!-- Add more statistics here -->
+                    </div>
+                </div>
                 <div class="text-center max-w-4xl mx-auto p-4 mt-8 text-lg text-slate-800 dark:text-white/80">
                     <p>
                         In the early part of this year, I made a spontaneous decision to buy a fat tire bicycle. It
@@ -98,8 +130,8 @@ middleware(['redirect-to-dashboard']);
                             Check Out My Recent Rides!
                         </h2>
                         <main
-                            class="bg-white dark:bg-gray-800"
-                            x-data="{'layout': 'list'}"
+                                class="bg-white dark:bg-gray-800"
+                                x-data="{'layout': 'list'}"
                         >
                             <nav class="p-4 text-gray-800 dark:text-white bg-white dark:bg-gray-800">
                                 Layout:
@@ -117,12 +149,12 @@ middleware(['redirect-to-dashboard']);
                                 </button>
                             </nav>
 
-                            <section class=" sm:grid-cols-4 gap-4"
+                            <section class=" sm:grid-cols-2 gap-4"
                                      x-bind:class="{'pb-4': layout === 'list', 'p-2 grid grid-cols-2': layout === 'grid'}">
 
-                                @foreach(\App\Models\Ride::all() as $ride)
+                                @foreach($this->rides as $ride)
                                     <div
-                                        x-bind:class="{'w-full m-4 mb-0': layout === 'list', 'w-full': layout === 'grid'}"
+                                            x-bind:class="{'w-full m-4 mb-0': layout === 'list', 'w-full': layout === 'grid'}"
                                     >
                                         <x-bike-joy.ride :ride="$ride"/>
                                     </div>
