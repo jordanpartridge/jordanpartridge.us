bd514213a2481f0d003737ffb6e3aae1904b7e07
Jordan <jordan.l.partridge@gmail.com>
2024-04-24 20:53:08 -0700

    kill I no need

--- app/Console/Commands/SyncActivities.php
+++ app/Console/Commands/SyncActivities.php
@@ -10,10 +10,10 @@ use App\Models\StravaToken;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 use Saloon\Http\Response;
 use Illuminate\Console\Command;
 
-use function Laravel\Prompts\note;
 use function Laravel\Prompts\table;
 
 use function Laravel\Prompts\info;
@@ -58,36 +58,32 @@ class SyncActivities extends Command
                 $strava = new Strava($token->access_token);
                 $moreDataResponse = $strava->send(new ActivityRequest($activity['id']));
                 $activity['calories'] = $moreDataResponse->json()['calories'];
+                Ride::query()->updateOrCreate([
+                    'external_id' => $activity['external_id'],
+                ], [
+                    'date'          => Carbon::parse($activity['start_date_local']),
+                    'name'          => $activity['name'],
+                    'distance'      => $activity['distance'],
+                    'polyline'      => $activity['map']['summary_polyline'],
+                    'max_speed'     => $activity['max_speed'],
+                    'calories'      => $activity['calories'],
+                    'elevation'     => $activity['total_elevation_gain'],
+                    'average_speed' => $activity['average_speed'],
+                    'moving_time'   => $activity['moving_time'],
+                    'elapsed_time'  => $activity['elapsed_time'],
+                ]);
                 return $activity;
             });
 
             $this->info('Rides to Sync: ' . count($activities));
 
-            $this->storeResults($activities);
-            $this->displayRides($activities);
+
 
             $response->onError(function ($response) use ($token) {
-                $response->json();
-                table(
-                    ['status', 'message', 'token', 'field', 'Code'],
-                    [
-                        [
-                            $response->status(),
-                            $response->json()['message'],
-                            $token->access_token,
-                            $response->json()['errors'][0]['field'],
-                            $response->json()['errors'][0]['code'],
-                        ],
-                    ],
-                );
-
-                if ($response->status() === 401) {
-                    $this->info('Refreshing token: ' . $token->access_token);
-                    $token = $this->refreshToken($token);
-                    $this->info('Token refreshed :' . $token->access_token);
-                    $response = $this->getActivities($token);
-                    $this->storeResults($response);
-                }
+                Log::error('Error syncing activities', [
+                    'token'    => $token->access_token,
+                    'response' => $response->json(),
+                ]);
             });
         });
     }
@@ -101,36 +97,6 @@ class SyncActivities extends Command
         $activities = new AthleteActivityRequest();
 
         return $strava->send($activities);
-    }
-
-    private function refreshToken(StravaToken $token): StravaToken
-    {
-        $this->info('Refreshing token');
-
-        $strava = new Strava();
-        $response = $strava->refreshToken($token->refresh_token);
-        $this->info('Response: ' . $response->status());
-        if ($response->status() !== 200) {
-            $this->error('Token not refreshed');
-            return $token;
-        }
-
-        $token->update([
-            'access_token'  => $response->json()['access_token'],
-            'expires_at'    => now()->addSeconds($response->json()['expires_in']),
-            'refresh_token' => $response->json()['refresh_token'],
-        ]) ? $this->info('Token refreshed') : $this->error('Token not refreshed');
-
-        return $token->fresh();
-    }
-
-    private function displayFunctionSummary(string $functionName, array $parameters): void
-    {
-        note($functionName . ' function called with the following parameters:');
-        collect($parameters)->each(function ($value, $key) {
-            $this->info($key . ':');
-            $value->promptTable();
-        });
     }
 
     private function displayRides(Collection $rides): void
@@ -156,25 +122,4 @@ class SyncActivities extends Command
         );
     }
 
-
-    private function storeResults(Collection $activities): void
-    {
-        $activities->where('type', 'Ride')->each(function ($activity) {
-            Ride::query()->updateOrCreate([
-                'external_id' => $activity['external_id'],
-            ], [
-                'date'          => Carbon::parse($activity['start_date_local']),
-                'name'          => $activity['name'],
-                'distance'      => $activity['distance'],
-                'polyline'      => $activity['map']['summary_polyline'],
-                'max_speed'     => $activity['max_speed'],
-                'calories'      => $activity['calories'],
-                'elevation'     => $activity['total_elevation_gain'],
-                'average_speed' => $activity['average_speed'],
-                'moving_time'   => $activity['moving_time'],
-                'elapsed_time'  => $activity['elapsed_time'],
-            ]);
-        });
-        info('Activities synced');
-    }
 }
