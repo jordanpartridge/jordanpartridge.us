f9c2d07e580617a0376c608e153c2c5a876ce7b8
Jordan <jordan.l.partridge@gmail.com>
2024-05-04 10:05:37 -0700

    🚵🏾‍♀️ rides and statics are dynamic based off dates

--- tests/Feature/HttpTest.php
+++ tests/Feature/HttpTest.php
@@ -9,3 +9,8 @@ it('can see engineering page', function () {
     $response = $this->get('/engineering');
     $response->assertStatus(200);
 });
+
+it('can see bike page', function () {
+    $response = $this->get('/bike');
+    $response->assertStatus(200);
+});
