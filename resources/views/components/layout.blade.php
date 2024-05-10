5dcf213dad415f9b928841b7692fca3580149430
Jordan <jordan.l.partridge@gmail.com>
2024-05-09 22:18:29 -0700

    stuff for horizon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,10 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{config('app.name')}}</title>
-    <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css"
-    >
+
     <script src="https://kit.fontawesome.com/d1901f5db9.js" crossorigin="anonymous"></script>
 </head>
 <body>
