511a9ee52b12fe940a2cd2eb5b34f48a893eb06d
Jordan <jordan.l.partridge@gmail.com>
2024-05-12 22:05:28 -0700

    improve sync

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('strava:token-refresh')->everyFourHours();
+Schedule::command('strava:token-refresh')->everyFourHours()->withoutOverlapping();
 
-Schedule::command('sync')->everyFifteenMinutes();
+Schedule::command('sync')->everyFifteenMinutes()->withoutOverlapping();
