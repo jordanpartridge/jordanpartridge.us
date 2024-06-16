d5aa16d102c876a7bac239e8c6b9e2bef47f5e5b
Jordan <jordan.l.partridge@gmail.com>
2024-06-16 12:53:18 -0700

    small changes

--- resources/views/components/custom-login-link.blade.php
+++ resources/views/components/custom-login-link.blade.php
@@ -2,8 +2,7 @@
     @env('local')
         @if(Auth::check() === false)
         <div class="space-y-2">
-            <x-login-link class="p-3 text-gray-700 dark:text-gray-300" :email="$email"/>
-                          label="Login"/>
+            <x-login-link class="p-3 text-gray-700 dark:text-gray-300" :email="$email" label="Login"/>
         </div>
         @endif
     @endenv
