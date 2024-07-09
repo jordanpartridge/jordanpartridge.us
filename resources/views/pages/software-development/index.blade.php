5af3f57fb711dd161522e8a6e2586607a6a2c1e1
Jordan <jordan.l.partridge@gmail.com>
2024-07-08 22:13:30 -0700

    add bug maker

--- resources/views/pages/software-development/index.blade.php
+++ resources/views/pages/software-development/index.blade.php
@@ -56,7 +56,7 @@
                                   d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
                                   clip-rule="evenodd"></path>
                         </svg>
-                        Bug Hunter
+                        Bug Maker
                     </div>
                     <div
                         class="bg-gray-200 dark:bg-gray-700 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-300 flex items-center">
