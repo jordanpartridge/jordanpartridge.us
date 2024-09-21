9f9a40f79908b3782791fecd09e428e70a335b2e
Jordan <jordan.l.partridge@gmail.com>
2024-09-21 12:02:57 -0700

    tweak redirect controller

--- app/Http/Controllers/Strava/RedirectController.php
+++ app/Http/Controllers/Strava/RedirectController.php
@@ -6,17 +6,6 @@ class RedirectController
 {
     public function __invoke()
     {
-        $client_id = config('services.strava.client_id');
-        $redirect_uri = route('strava:callback');
-
-        if (! $client_id) {
-            return response('Strava client id not set', 500);
-        }
-
-        if (! $redirect_uri) {
-            return response('route: `strava:callback` doe not exist', 500);
-        }
-
         $query = http_build_query([
             'client_id'     => config('services.strava.client_id'),
             'redirect_uri'  => route('strava:callback'),
