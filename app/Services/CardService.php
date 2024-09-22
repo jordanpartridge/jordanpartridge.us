f927edc3d068eb7fc036ab438aa84c12fcbc7acc
Jordan <jordan.l.partridge@gmail.com>
2024-09-21 21:50:20 -0700

    game creation logic in place

--- app/Services/CardService.php
+++ app/Services/CardService.php
@@ -8,6 +8,7 @@ use App\Http\Integrations\CardApi\Requests\DrawCard;
 use App\Http\Integrations\CardApi\Requests\GetDeck;
 use Exception;
 use RuntimeException;
+use Saloon\Http\Response;
 
 final readonly class CardService
 {
@@ -15,37 +16,38 @@ final readonly class CardService
     {
     }
 
-    public function drawCard(): array
+    public function drawCard(string $name): array
     {
         $deck = $this->getDeck();
 
-        return $this->cardApi->send(new DrawCard($this->deckName))->json();
+        return $this->cardApi->send(new DrawCard($name))->json();
     }
 
-    public function createDeck(string $name): array
+    public function createDeck(string $name): Response
     {
         try {
-            $deck = $this->cardApi->send(new CreateDeck($name))->json();
+            $deck = $this->cardApi->send(new CreateDeck($name));
         } catch (Exception $e) {
             throw new RuntimeException('Failed to create deck: ' . $e->getMessage());
         }
+
         return $deck;
     }
 
-    public function getDeck($name = null): array
+    public function getDeck(string $name): Response
     {
-        return $this->cardApi->send(new GetDeck($name ?? $this->deckName))->json();
+        return $this->cardApi->send(new GetDeck($name));
     }
 
     public function initializeDeck(string $name): array
     {
         try {
-            $existingDeck = $this->getDeck($name);
-            if (isset($existingDeck['message']) && $existingDeck['message'] === 'Not Found.') {
-                return $this->createDeck($name);
+            $deck = $this->getDeck($name);
+            if ($deck->status() === 404) {
+                $deck = $this->createDeck($name);
             }
 
-            return $existingDeck;
+            return $deck->json();
         } catch (Exception $e) {
             throw new RuntimeException('Failed to initialize deck: ' . $e->getMessage());
         }
--- tests/Feature/GameTest.php
+++ tests/Feature/GameTest.php
@@ -3,11 +3,18 @@
 use App\Models\Game;
 use Illuminate\Support\Carbon;
 
-it('has a factory, with default in sync with the migrations', function () {
+it('can successfully be created with factory default settings.', function () {
     $game = Game::factory()->create();
     expect($game->deck_slug)->toBeString()
         ->and($game->name)->toBeString()
         ->and($game->created_at)->toBeInstanceOf(Carbon::class)
         ->and($game->updated_at)->toBeInstanceOf(Carbon::class)
         ->and($game->deleted_at)->toBeNull();
+});
+
+it('belongs to many players (users).', function () {
+    $game = Game::factory()->create();
+    $game->players()->create(['name' => 'John Doe', 'email' => 'john@example.com', 'password' => bcrypt('password')]);
+    $game->players()->create(['name' => 'Jane Doe', 'email' => 'jane@example.com', 'password' => bcrypt('password')]);
+    expect($game->players()->count())->toBe(2);
 });
