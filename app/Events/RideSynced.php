23d49ec65cf3b7848771716da93b6baf02c9f9ac
Jordan <jordan.l.partridge@gmail.com>
2024-09-24 17:02:58 -0700

    finish refactor

--- app/Events/RideSynced.php
+++ app/Events/RideSynced.php
@@ -10,32 +10,32 @@ use Thunk\Verbs\Event;
 
 class RideSynced extends Event
 {
-    public array $data;
+    public array $ride;
 
     public function handle(): void
     {
         $ride = Ride::query()->updateOrCreate(
-            ['external_id' => $activity['external_id']],
+            ['external_id' => $this->ride['external_id']],
             [
-                 'date'          => Carbon::parse($activity['start_date_local']),
-                 'name'          => $activity['name'],
-                 'distance'      => $activity['distance'],
-                 'polyline'      => $activity['map']['summary_polyline'],
-                 'map_url'       => $activity['map_url'],
-                 'max_speed'     => $activity['max_speed'],
-                 'calories'      => $activity['calories'],
-                 'elevation'     => $activity['total_elevation_gain'],
-                 'average_speed' => $activity['average_speed'],
-                 'moving_time'   => $activity['moving_time'],
-                 'elapsed_time'  => $activity['elapsed_time'],
-             ]
+                'date'          => Carbon::parse($this->ride['start_date_local']),
+                'name'          => $this->ride['name'],
+                'distance'      => $this->ride['distance'],
+                'polyline'      => $this->ride['map']['summary_polyline'],
+                'map_url'       => $this->ride['map_url'],
+                'max_speed'     => $this->ride['max_speed'],
+                'calories'      => $this->ride['calories'],
+                'elevation'     => $this->ride['total_elevation_gain'],
+                'average_speed' => $this->ride['average_speed'],
+                'moving_time'   => $this->ride['moving_time'],
+                'elapsed_time'  => $this->ride['elapsed_time'],
+            ]
         );
 
         User::first()->notify(new RideSyncedNotification($ride));
         activity('bikes')
             ->event('synced')
             ->on($ride)
-            ->withProperties(['ride' => $this->data])
+            ->withProperties(['name' => $this->ride['name'], 'distance' => $this->ride['distance']])
             ->log('Ride synced');
     }
 }
