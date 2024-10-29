fe15844509be83a4eb8b6135c6e78829509e308e
Jordan Partridge <jordan.l.partridge@gmail.com>
2024-10-28 23:31:11 -0700

    feature/remove more strava (#99)
    * remove refresh token as it's no longer needed
    
    * update strava-client

--- routes/console.php
+++ routes/console.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('strava:token-refresh')->hourly()->withoutOverlapping();
-
 Schedule::command('sync')->hourly()->withoutOverlapping();
 
 Schedule::command('activitylog:clean')->daily();
