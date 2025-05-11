3e617db7e4c90b0a0e943e624c5eb23d5672bffb
Jordan Partridge <jordan@partridge.rocks>
2025-05-11 14:57:42 -0700

    Fix RSS feed XML declaration using proper Laravel syntax

--- resources/views/pages/feed/xml.blade.php
+++ resources/views/pages/feed/xml.blade.php
@@ -1,4 +1,4 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<?= '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL ?>
 <rss version="2.0"
      xmlns:content="http://purl.org/rss/1.0/modules/content/"
      xmlns:atom="http://www.w3.org/2005/Atom">
