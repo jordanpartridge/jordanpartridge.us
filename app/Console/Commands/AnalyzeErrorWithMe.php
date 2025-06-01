04dd90b4b8bd2377157424c0f7311fc57b1df0ee
Jordan Partridge <jordan@partridge.rocks>
2025-06-01 03:44:40 +0000

    Fix duplicate Process import in AnalyzeErrorWithMe command

--- app/Console/Commands/AnalyzeErrorWithMe.php
+++ app/Console/Commands/AnalyzeErrorWithMe.php
@@ -5,7 +5,6 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Process;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Process;
 
 class AnalyzeErrorWithMe extends Command
 {
